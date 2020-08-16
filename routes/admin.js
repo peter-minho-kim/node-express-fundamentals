8bdb47203bd49de9a173f212c25665f33383099c
Peter Kim <peter.minho.kim@gmail.com>
2020-08-16 13:33:48 -0400

    Finish pug template

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
+
+const express = require('express');
 
 const rootDir = require('../util/path');
+
+const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
