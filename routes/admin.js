a16b4ce6ce35b8ad0ad44444612161abb2be3d78
Peter Kim <peter.minho.kim@gmail.com>
2020-08-15 20:37:20 -0400

    Filter paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">SUBMIT</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">SUBMIT</button></form>'
   );
 });
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
