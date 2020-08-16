3d2559d527f9b1b002f8f9df78dffb8bd72e3d1f
Peter Kim <peter.minho.kim@gmail.com>
2020-08-15 20:31:38 -0400

    Add express router

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop');
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">SUBMIT</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello World</h1>');
-});
 
 app.listen(3000);
