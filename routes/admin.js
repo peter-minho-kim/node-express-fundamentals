be7ccb636c21e0af99f546a1bafa79ccb4518706
Peter Kim <peter.minho.kim@gmail.com>
2020-08-16 10:58:46 -0400

    Share data across requests

--- routes/admin.js
+++ routes/admin.js
@@ -4,15 +4,18 @@ const router = express.Router();
 
 const rootDir = require('../util/path');
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
-router.post('/product', (req, res, next) => {
-  console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
